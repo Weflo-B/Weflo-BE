ea6f1c754d1d5ae57be52e82b1b85a0edcd58b56
bbbang105 <2018111366@dgu.ac.kr>
2024-03-06 14:45:20 +0900

    [feat] : 특정 요청에 따른 드론 정보 DTO

---
+++ src/main/java/Weflo/backend/dto/drone/response/DroneResponse.java
@@ -0,0 +1,17 @@
+package Weflo.backend.dto.drone.response;
+
+import Weflo.backend.dto.common.DroneInfoDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class DroneResponse {
+    /*
+    private List<DroneInfoDto> droneInfos;
+    */
+}
