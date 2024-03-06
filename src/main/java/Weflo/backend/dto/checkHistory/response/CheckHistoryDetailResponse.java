9295ad7a0bd30d89c2876522111be9eeda69da0f
bbbang105 <2018111366@dgu.ac.kr>
2024-03-06 14:43:13 +0900

    [feat] : 구동부 점수까지 나오는 테스트 상세 내역 DTO

---
+++ src/main/java/Weflo/backend/dto/checkHistory/response/CheckHistoryDetailResponse.java
@@ -0,0 +1,20 @@
+package Weflo.backend.dto.checkHistory.response;
+
+import Weflo.backend.dto.common.PartScoreDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CheckHistoryDetailResponse {
+    /*
+    private int totalScore;
+    private String checkDate;
+    private String location;
+    private List<PartScoreDto> partsScore;
+    */
+}
