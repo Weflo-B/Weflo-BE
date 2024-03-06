e28d23dd74f4a5e924781b39dc79e3f36c816524
bbbang105 <2018111366@dgu.ac.kr>
2024-03-06 14:29:43 +0900

    [feat] : 유저 로그인 DTO

---
+++ src/main/java/Weflo/backend/dto/user/request/LoginRequest.java
@@ -0,0 +1,16 @@
+/*
+package Weflo.backend.dto.user.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class LoginRequest {
+    private String loginId;
+    private String loginPassword;
+}
+*/
---
+++ src/main/java/Weflo/backend/dto/user/response/LoginResponse.java
@@ -0,0 +1,15 @@
+/*
+package Weflo.backend.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class LoginResponse {
+    private Long id;
+    private String jwtToken;
+}
+*/
