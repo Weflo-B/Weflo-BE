7d3a89ee6c14d41d083ce7804aace0099e1c7f7c
chahyunsoo <chahs1012@naver.com>
2024-03-08 15:21:55 +0900

    [feat] : 견적서 조회 서비스 로직 추가

--- src/main/java/Weflo/backend/service/orderpart/OrderPartDetailService.java
+++
@@ -1,7 +0,0 @@
-//package Weflo.backend.service.orderpart;
-//
-//import Weflo.backend.dto.part.response.OrderPartsDetailResponse;
-//
-//public interface OrderPartDetailService {
-//    OrderPartsDetailResponse getOrderPartDetailOfDrone(Long userId, Long droneId);
-//}
---
+++ src/main/java/Weflo/backend/service/orderpart/QuotationDetailService.java
@@ -0,0 +1,8 @@
+package Weflo.backend.service.orderpart;
+
+import Weflo.backend.dto.part.response.OrderPartsDetailResponse;
+import Weflo.backend.dto.quotation.response.QuotationResponse;
+
+public interface QuotationDetailService {
+    QuotationResponse getQuotationDetailOfDrone(Long userId);
+}
