c7f400051fd038c34207be5d7c1b7e04421d48a5
bbbang105 <2018111366@dgu.ac.kr>
2024-03-08 15:20:04 +0900

    [feat] : 배송 상세 내역 DTO 기능 추가

--- src/main/java/Weflo/backend/dto/deliveryDetail/response/DeliveryDetailResponse.java
+++ src/main/java/Weflo/backend/dto/deliveryDetail/response/DeliveryDetailResponse.java
@@ -1,18 +1,23 @@
 package Weflo.backend.dto.deliveryDetail.response;
 
+import Weflo.backend.dto.common.BuyerInfoDto;
+import Weflo.backend.dto.common.PastDateInfoDto;
+import Weflo.backend.dto.common.ProductInfoDto;
+import Weflo.backend.dto.common.SellerInfoDto;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Builder
 public class DeliveryDetailResponse {
     private LocalDate orderDate;
     private LocalDate estimateDate;
-    private String deliveryDetailStatus;
-    private String category;
-    private String name;
-    private int salePrice;
-    private int amount;
+    private String currentStatus;
+    private List<PastDateInfoDto> pastDates;
+    private ProductInfoDto productInfos;
+    private SellerInfoDto sellerInfos;
+    private BuyerInfoDto buyerInfos;
 }
