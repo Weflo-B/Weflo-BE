b89239d6b1f89e8bd2dc6fa35fd249174667f1c4
chahyunsoo <chahs1012@naver.com>
2024-03-08 13:55:44 +0900

    [feat] : 부품 주문 Repository 모두 수정

--- src/main/java/Weflo/backend/repository/drone/DroneRepository.java
+++ src/main/java/Weflo/backend/repository/drone/DroneRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DroneRepository extends JpaRepository<Drone, Long> {
     List<Drone> findAllByUserId(Long userId);
+    Optional<Drone> findByIdAndUserId(Long droneId, Long userId);
+
+
 }
