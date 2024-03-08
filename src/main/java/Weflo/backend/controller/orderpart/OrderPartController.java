5bc6a404bea3d74e200a4bf896a22a9dd8f0285b
chahyunsoo <chahs1012@naver.com>
2024-03-08 13:56:08 +0900

    [feat] : 부품 주문 전체 조회, 단건 조회 api 구현
    id 중복 문제 해결

--- src/main/java/Weflo/backend/controller/orderpart/OrderPartController.java
+++ src/main/java/Weflo/backend/controller/orderpart/OrderPartController.java
@@ -1,7 +1,11 @@
 package Weflo.backend.controller.orderpart;
 
+import Weflo.backend.dto.common.OrderPartsDto;
 import Weflo.backend.dto.part.response.AllOrderPartsResponse;
+import Weflo.backend.dto.part.response.OrderPartsDetailResponse;
 import Weflo.backend.global.ApiResponse;
+//import Weflo.backend.service.orderpart.OrderPartDetailService;
+//import Weflo.backend.service.orderpart.OrderPartDetailService;
 import Weflo.backend.service.orderpart.OrderPartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +20,20 @@ import java.util.List;
 @RequestMapping(value = "/api")
 public class OrderPartController {
     private final OrderPartService orderPartService;
+//    private final OrderPartDetailService orderPartDetailService;
 
     @GetMapping("/orders/{userId}")
     public ApiResponse<AllOrderPartsResponse> getAllOrderParts(@PathVariable Long userId) {
         AllOrderPartsResponse allOrderParts = orderPartService.getAllOrderParts(userId);
         return ApiResponse.onSuccess(allOrderParts);
     }
+
+    @GetMapping("/orders/{userId}/{droneId}")
+    public ApiResponse<OrderPartsDetailResponse> getOrderParts(@PathVariable Long userId,
+                                                               @PathVariable Long droneId) {
+        OrderPartsDetailResponse orderPartDetailOfDrone = orderPartService.getOrderPart(userId, droneId);
+        return ApiResponse.onSuccess(orderPartDetailOfDrone);
+    }
 }
+
+
