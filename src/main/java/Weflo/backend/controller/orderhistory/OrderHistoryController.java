f1eb504cfda33ab1451c19b58d54786c10bca773
bbbang105 <2018111366@dgu.ac.kr>
2024-03-08 13:14:02 +0900

    [feat] : OrderHistory 컨트롤러 기능 구현

--- src/main/java/Weflo/backend/controller/orderhistory/OrderHistoryController.java
+++ src/main/java/Weflo/backend/controller/orderhistory/OrderHistoryController.java
@@ -1,12 +1,26 @@
 package Weflo.backend.controller.orderhistory;
 
+import Weflo.backend.dto.orderHistory.response.AllOrderHistoriesResponse;
+import Weflo.backend.global.ApiResponse;
+import Weflo.backend.global.status.Message;
+import Weflo.backend.service.orderHistory.OrderHistoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/histories")
 public class OrderHistoryController {
+    private final OrderHistoryService orderHistoryService;
 
+    @GetMapping
+    public ApiResponse<AllOrderHistoriesResponse> getOrderHistories(
+            @RequestParam("user_id") Long userId,
+            @RequestParam(value = "month", required = false) Integer month,
+            @RequestParam(value = "order_status", required = false) String orderStatus) {
+
+        AllOrderHistoriesResponse allOrderHistoriesResponse = orderHistoryService.getOrderHistories(userId, month, orderStatus);
+
+        return ApiResponse.onSuccess(Message._GET_ALL_ORDER_HISTORIES_MESSAGE.getMessage(), allOrderHistoriesResponse);
+    }
 }
