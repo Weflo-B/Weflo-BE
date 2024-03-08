049c83773dc77d3b8974aaa9c84da619fe8cdf98
bbbang105 <2018111366@dgu.ac.kr>
2024-03-08 13:10:54 +0900

    [feat] : DTO 변수 추가

--- src/main/java/Weflo/backend/dto/common/OrderHistoryDto.java
+++ src/main/java/Weflo/backend/dto/common/OrderHistoryDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -13,5 +15,7 @@ public class OrderHistoryDto {
     private String name;
     private Integer salePrice;
     private Integer amount;
+    private LocalDate orderDate;
     private String status;
+    private String productImage;
 }
