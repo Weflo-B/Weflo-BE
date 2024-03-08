5bf3efcbbb833a5beb4479b0254dee3c30695a72
chahyunsoo <chahs1012@naver.com>
2024-03-08 21:16:53 +0900

    [refactor] : 부품 주문 전체 조회시 부품 주문 갯수도 함께 반환하게 수정

--- src/main/java/Weflo/backend/dto/part/response/AllOrderPartsResponse.java
+++ src/main/java/Weflo/backend/dto/part/response/AllOrderPartsResponse.java
@@ -1,19 +1,27 @@
 package Weflo.backend.dto.part.response;
 
 import Weflo.backend.dto.common.OrderPartsDto;
+import Weflo.backend.dto.common.ProductInfoDto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Data
 @Builder
 public class AllOrderPartsResponse {
     private List<OrderPartsDto> orderParts;
+    private Integer totalOrderCount;
 
     public static AllOrderPartsResponse of(List<OrderPartsDto> orderPartsDtoList) {
+        int totalOrderCount = orderPartsDtoList.stream()
+                .mapToInt(orderPartsDto -> orderPartsDto.getProductsInfo().stream()
+                        .mapToInt(ProductInfoDto::getAmount).sum()).sum();
+
         return builder()
                 .orderParts(orderPartsDtoList)
+                .totalOrderCount(totalOrderCount)
                 .build();
     }
 }
