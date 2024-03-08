41b54f96f37a95a741565798a8b6188309fc3da1
chahyunsoo <chahs1012@naver.com>
2024-03-08 16:01:38 +0900

    [feat] : 부품 주문 진행 상태 변경 api 추가

--- src/main/java/Weflo/backend/controller/quotation/QuotationController.java
+++ src/main/java/Weflo/backend/controller/quotation/QuotationController.java
@@ -2,23 +2,25 @@ package Weflo.backend.controller.quotation;
 
 import Weflo.backend.dto.quotation.response.QuotationResponse;
 import Weflo.backend.global.ApiResponse;
-import Weflo.backend.service.orderpart.OrderPartService;
 import Weflo.backend.service.orderpart.QuotationDetailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api")
 public class QuotationController {
-    //    private final OrderPartService orderPartService;
     private final QuotationDetailService quotationDetailService;
 
-    @GetMapping("/quotation/{user_id}")
-    public ApiResponse<QuotationResponse> getQuotation(@PathVariable Long user_id) {
-        return ApiResponse.onSuccess(quotationDetailService.getQuotationDetailOfDrone(user_id));
+    @GetMapping("/quotation/{userId}")
+    public ApiResponse<QuotationResponse> getQuotation(@PathVariable Long userId) {
+        return ApiResponse.onSuccess(quotationDetailService.getQuotationDetailOfDrone(userId));
+    }
+
+    @PatchMapping("/orders/{userId}")
+    public ApiResponse<String> confirmOrder(@PathVariable Long userId) {
+        quotationDetailService.confirmOrder(userId);
+        return ApiResponse.onSuccess("부품 주문에 성공했습니다. 주문 완료!");
     }
 }
