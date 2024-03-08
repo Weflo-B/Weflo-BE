bc9f1ef19b6d5a4b7225401e0ad13450f3311f6e
chahyunsoo <chahs1012@naver.com>
2024-03-08 17:11:51 +0900

    [fix] : orderhistory 관련 코드 수정

--- src/main/java/Weflo/backend/repository/orderhistory/OrderHistoryRepository.java
+++ src/main/java/Weflo/backend/repository/orderhistory/OrderHistoryRepository.java
@@ -1,15 +1,17 @@
 package Weflo.backend.repository.orderHistory;
 
-
 import Weflo.backend.domain.Drone;
 import Weflo.backend.domain.OrderHistory;
-import org.hibernate.query.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
     List<OrderHistory> findByDrone(Drone droneId);
+    List<OrderHistory> findAllByDroneId(Long droneId);
+    List<OrderHistory> findAllByDroneIdAndOrderHistoryStatus(Long drone_id, String orderHistoryStatus);
+    List<OrderHistory> findAllByDroneIdAndOrderDateBetween(Long droneId, LocalDate startOfMonth, LocalDate endOfMonth);
+    List<OrderHistory> findAllByDroneIdAndOrderDateBetweenAndOrderHistoryStatus(Long droneId, LocalDate startOfMonth, LocalDate endOfMonth, String orderHistoryStatus);
+
 }
